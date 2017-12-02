ec19e11b6250a5e3e5cd2f0b63ea735f1fc963e9
Marco Barbosa <mba@ch.flsystem.net>
2017-12-02 12:25:21 +0100

    Added Scene

--- src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
+++ src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
@@ -14,14 +14,13 @@ public class PlayerController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetAxisRaw("Horizontal") > 0.1f  || Input.GetAxisRaw("Horizontal") < -0.1f)
+	    if (Input.GetAxisRaw("Horizontal") > 0.1  || Input.GetAxisRaw("Horizontal") < -0.1)
 	    {
-	        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+	        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0));
 	    }
-	    if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
+	    if (Input.GetAxisRaw("Vertical") > 0.1 || Input.GetAxisRaw("Vertical") < -0.1)
 	    {
-	        transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime ,0f));
+	        transform.Translate(new Vector3(0, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime ,0));
 	    }
-
     }
 }
