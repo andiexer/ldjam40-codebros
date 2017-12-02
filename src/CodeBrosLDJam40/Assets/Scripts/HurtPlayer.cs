603da2a332810b41c74794aeead3e4f6f2a62b44
Andi Exer <andi.exer@windowslive.com>
2017-12-02 21:09:00 +0100

    added map, velocity and player health system

---
+++ src/CodeBrosLDJam40/Assets/Scripts/HurtPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealthManager>().HurtPlayer(Random.Range(0,10));
+        }
+    }
+}
--- src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
+++ src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
@@ -10,30 +10,45 @@ public class PlayerController : MonoBehaviour
     private Animator _anim;
     private bool _playerMoving;
     private Vector3 _lastMove;
+    private Rigidbody2D _rigidbody2D;
 
 	// Use this for initialization
 	void Start ()
 	{
 	    _anim = GetComponent<Animator>();
+	    _rigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	    _playerMoving = false;
-
-        if (Input.GetAxisRaw("Horizontal") > 0.1  || Input.GetAxisRaw("Horizontal") < -0.1)
+    
+	    if (Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
 	    {
-	        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0));
-	        _playerMoving = true;
+	        //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            _rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, _rigidbody2D.velocity.y);
+            _playerMoving = true;
             _lastMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
-	    }
-	    if (Input.GetAxisRaw("Vertical") > 0.1 || Input.GetAxisRaw("Vertical") < -0.1)
-	    {
-	        transform.Translate(new Vector3(0, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime ,0));
-	        _playerMoving = true;
-	        _lastMove = new Vector3(0, Input.GetAxisRaw("Vertical"));
         }
+        if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
+        {
+            //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+            _playerMoving = true;
+            _lastMove = new Vector3(0, Input.GetAxisRaw("Vertical"));
+        }
+
+	    if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+	    {
+	        _rigidbody2D.velocity = new Vector2(0f, _rigidbody2D.velocity.y);
+	    }
+
+	    if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+	    {
+	        _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0f );
+	    }
+
 
         _anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         _anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
