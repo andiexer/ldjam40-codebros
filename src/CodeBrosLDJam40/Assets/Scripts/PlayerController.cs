802d4d07a1148f2fe749de7f9c2c005ff657467b
Andi Exer <andi.exer@windowslive.com>
2017-12-02 15:05:55 +0100

    commit with no issues hopefully

--- src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
+++ src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
@@ -7,20 +7,38 @@ public class PlayerController : MonoBehaviour
 
     public float moveSpeed;
 
+    private Animator _anim;
+    private bool _playerMoving;
+    private Vector3 _lastMove;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+	    _anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetAxisRaw("Horizontal") > 0.1  || Input.GetAxisRaw("Horizontal") < -0.1)
+
+	    _playerMoving = false;
+
+        if (Input.GetAxisRaw("Horizontal") > 0.1  || Input.GetAxisRaw("Horizontal") < -0.1)
 	    {
 	        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0));
+	        _playerMoving = true;
+            _lastMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
 	    }
 	    if (Input.GetAxisRaw("Vertical") > 0.1 || Input.GetAxisRaw("Vertical") < -0.1)
 	    {
 	        transform.Translate(new Vector3(0, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime ,0));
-	    }
+	        _playerMoving = true;
+	        _lastMove = new Vector3(0, Input.GetAxisRaw("Vertical"));
+        }
+
+        _anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        _anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        _anim.SetBool("PlayerMoving", _playerMoving);
+        _anim.SetFloat("LastMoveX", _lastMove.x);
+	    _anim.SetFloat("LastMoveY", _lastMove.y);
     }
 }
