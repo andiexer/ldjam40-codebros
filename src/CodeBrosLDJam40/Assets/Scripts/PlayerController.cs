ac5921d2e1b60c8b478f44d44a20baa656e8b2d5
Andi Exer <andi.exer@windowslive.com>
2017-12-02 11:44:41 +0100

    added base movements without animations

---
+++ src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetAxisRaw("Horizontal") > 0.1f  || Input.GetAxisRaw("Horizontal") < -0.1f)
+	    {
+	        transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+	    }
+	    if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
+	    {
+	        transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime ,0f));
+	    }
+
+    }
+}
