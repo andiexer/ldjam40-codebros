b83b3efea725a81c6c33a5397a196cb5e9583078
Marco Barbosa <marcobarbosa@outlook.com>
2017-12-03 15:33:56 +0100

    V1 of Game

---
+++ src/CodeBrosLDJam40/Assets/Scripts/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float xSpeed = 0.1f;
+    public float ySpeed = 0.1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(DestroyBullet());
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 position = transform.position;
+        position.x += xSpeed;
+        position.y += ySpeed;
+        transform.position = position;
+
+    }
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+}
---
+++ src/CodeBrosLDJam40/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    private Animator _anim;
+    private bool _playerMoving;
+    private bool _attacking;
+    public float AttackTime;
+    private float _attackTimeCounter;
+    private Vector3 _lastMove;
+    private Rigidbody2D _rigidbody2D;
+
+    // Use this for initialization
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        _playerMoving = false;
+
+        if (!_attacking)
+        {
+            if (Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f)
+            {
+                //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+                _rigidbody2D.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, _rigidbody2D.velocity.y);
+                _playerMoving = true;
+                _lastMove = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+            }
+            if (Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Vertical") < -0.1f)
+            {
+                //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
+                _playerMoving = true;
+                _lastMove = new Vector3(0, Input.GetAxisRaw("Vertical"));
+            }
+
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+            {
+                _rigidbody2D.velocity = new Vector2(0f, _rigidbody2D.velocity.y);
+            }
+
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            {
+                _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0f);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _attackTimeCounter = AttackTime;
+                _attacking = true;
+                _rigidbody2D.velocity = Vector2.zero;
+                _anim.SetBool("IsAttacking", _attacking);
+            }
+        }
+
+        if (_attackTimeCounter > 0)
+        {
+            _attackTimeCounter -= Time.deltaTime;
+        }
+
+        if (_attackTimeCounter <= 0)
+        {
+            _attacking = false;
+            _anim.SetBool("IsAttacking", false);
+        }
+
+        _anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
+        _anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        _anim.SetBool("PlayerMoving", _playerMoving);
+        _anim.SetFloat("LastMoveX", _lastMove.x);
+        _anim.SetFloat("LastMoveY", _lastMove.y);
+
+        /*added the fire code:
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (ammo <= 0)
+            {
+
+            }
+            else
+            {
+                GameObject go = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+                go.GetComponent<Bullet>().xSpeed = 0.1f;
+                ammo--;
+                Debug.Log("Ammo -1 " + ammo);
+            }
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (ammo <= 0)
+            {
+
+            }
+            else
+            {
+                GameObject go = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+                go.GetComponent<Bullet>().ySpeed = -0.1f;
+                ammo--;
+                Debug.Log("Ammo -1 " + ammo);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (ammo <= 0)
+            {
+
+            }
+            else
+            {
+                GameObject go = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+                go.GetComponent<Bullet>().ySpeed = 0.1f;
+                ammo--;
+                Debug.Log("Ammo -1 " + ammo);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (ammo <= 0)
+            {
+
+            }
+            else
+            {
+                GameObject go = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+                go.GetComponent<Bullet>().xSpeed = -0.1f;
+                ammo--;
+                Debug.Log("Ammo -1 " + ammo);
+            }
+        }*/
+    }
+}
